4c4d99b1d645ec80bbfd34d55bf6161d66a8ebc9
selketjah <gien.verschatse@gmail.com>
2016-02-18 20:51:12 +0100

    implementing quiz chain, fake twitter handler, some notes

--- QuizBot/QuizBot/Script.fsx
+++ QuizBot/QuizBot/Script.fsx
@@ -1,5 +1,8 @@
 ﻿#load "Core.fs"
+open QuizBot
 open QuizBot.Core
+#load "Twitter.fs"
+//open QuizBot.Twitter
 
 #I "../../packages"
 #r @"FSharp.Data/lib/net40/FSharp.Data.dll"
@@ -9,3 +12,27 @@ open QuizBot.WorldBankQuestions
 let country, capital = getRandomCountryCapital ()
 let q = { Question = country; ExpectedAnswer = capital }
 
+
+
+open System
+
+let sleepTime = TimeSpan(0,0,1)
+
+let rec loop (sleepTime:TimeSpan) = async {
+  printfn "started..."
+  // create question
+  let question = guessCapitalOfCountryQuestion()  
+  // post question
+  let questionID = Twitter.postTweet question.Question
+
+  do! Async.Sleep (sleepTime.TotalMilliseconds |> int)
+  // grab answers
+  let answers = Twitter.grabReplies questionID
+
+  answers |> List.iter (printfn "%A")
+  // check winner
+  
+  return! loop(sleepTime)
+}
+
+loop sleepTime |> Async.Start
---
+++ QuizBot/QuizBot/Twitter.fs
@@ -0,0 +1,19 @@
+﻿namespace QuizBot
+
+[<RequireQualifiedAccess>]
+module Twitter =
+
+  let postTweet question =
+
+    printfn "%s" question
+    
+    let id = 1234UL
+
+    id
+
+  let grabReplies (id:uint64) =
+      // ID of the response, username, answer
+      [
+        1UL, "@joe", "Brussels"
+        2UL, "@jack", "Paris"
+      ]
