69e3a401124900e6bc14844c635e746ee0d4f282
selketjah <gien.verschatse@gmail.com>
2016-02-19 15:38:27 +0100

    implemented post tweet

--- QuizBot/QuizBot/Twitter.fs
+++ QuizBot/QuizBot/Twitter.fs
@@ -3,13 +3,44 @@
 [<RequireQualifiedAccess>]
 module Twitter =
 
-  let postTweet question =
-
-    printfn "%s" question
+  open System
+  open System.Configuration
+  open System.Threading
+  open System.Threading.Tasks
+  open LinqToTwitter
     
-    let id = 1234UL
+  let appSettings = ConfigurationManager.AppSettings
+  let apiKey = appSettings.["apiKey"]
+  let apiSecret = appSettings.["apiSecret"]
+  let accessToken = appSettings.["accessToken"]
+  let accessTokenSecret = appSettings.["accessTokenSecret"]
 
-    id
+  let context = 
+    let credentials = SingleUserInMemoryCredentialStore()
+    credentials.ConsumerKey <- apiKey
+    credentials.ConsumerSecret <- apiSecret
+    credentials.AccessToken <- accessToken
+    credentials.AccessTokenSecret <- accessTokenSecret
+    let authorizer = SingleUserAuthorizer()
+    authorizer.CredentialStore <- credentials
+    new TwitterContext(authorizer)
+
+  let trimToTweet (msg:string) =
+    if msg.Length > 140 
+    then msg.Substring(0,134) + " [...]"
+    else msg
+
+  let postTweet question =
+    let message = 
+      question
+      |> trimToTweet
+
+    let status = 
+      Task.Run<Status>(fun _ -> 
+        question
+        |> context.TweetAsync)
+
+    status.Id
 
   let grabReplies (id:uint64) =
       // ID of the response, username, answer
