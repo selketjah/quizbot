f95d1775af04072d23e4d0545b10264309855606
selketjah <gien.verschatse@gmail.com>
2016-03-06 19:04:50 +0100

    added star wars questions

---
+++ QuizBot/QuizBot/StarWarsQuestions.fs
@@ -0,0 +1,17 @@
+﻿namespace QuizBot
+
+module StarWarsQuestions =
+
+  open StarWars.API
+  open QuizBot.Core
+
+  let movies = getAllFilms()
+
+  let planets =
+    getAllPlanets()
+    |> Array.map(fun m -> m.Name)
+    |> String.concat ", "
+
+  let guessThreePlanetsQuestion () =
+    { Question = sprintf "Name three planets from Star Wars. [1,2,3]"
+      ExpectedAnswer = planets }
